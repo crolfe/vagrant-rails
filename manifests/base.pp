af7ce7bd6ad7c654d25eb792833078f1af9556bc
Colin Rolfe <crolfe@gmail.com>
2013-03-20 20:34:51 +1100

    added module to install rails

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,4 +1,12 @@
 stage { 'first':
+    before => Stage['second']
+}
+
+stage { 'second': 
+    before => Stage['third']
+}
+
+stage { 'third':
     before => Stage['main']
 }
 
@@ -6,11 +14,20 @@ class { 'apt_get::update':
     stage => first,
 }
 
+class { 'ruby':
+    stage => second,
+}
+
+class { 'rails':
+    stage => third,
+}
+
 include apt_get::update
 include avahi-daemon
 include git
 include nodejs
 include postgres
+include rails
 include ruby
 include vim
 
---
+++ modules/rails/manifests/init.pp
@@ -0,0 +1,6 @@
+class rails {
+    package { 'rails':
+        ensure => latest,
+        provider => gem,
+    }
+}
